9b83472ad462c12447f9e89b189c7767cbf456cf
Viktorminator <viktorminator@gmail.com>
2014-12-09 15:23:50 +0200

    Concat JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 var coffeeSources = ['builds/production/coffee/*.coffee'];
+var jsSources = [
+	'builds/production/scripts/rclick.js',
+	'builds/production/scripts/pixgrid.js',
+	'builds/production/scripts/tagline.js',
+	'builds/production/scripts/template.js'
+];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true})
 			.on('error', gutil.log))
 		.pipe(gulp.dest('builds/production/scripts'))
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
