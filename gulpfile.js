a73e6b2c324e87132fe6fba440d5090134a787c5
Viktorminator <viktorminator@gmail.com>
2014-12-09 20:53:48 +0200

    Added jquery

--- gulpfile.js
+++ gulpfile.js
@@ -22,5 +22,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
